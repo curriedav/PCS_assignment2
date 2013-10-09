736b0d00a46ed276331640de5e511768dded6d6a
JFingert <joshuafingert@gmail.com>
2013-10-08 22:49:08 -0700

    weeek2_assignment1

---
+++ week2_assignment.js
@@ -0,0 +1,16 @@
+var string = "Marbles is a chicken";
+
+for (var i = string.length; i>=0; i--){
+
+  if(string[i]!= "a" && string[i]!= "e" && string[i]!= "i" && string[i]!= "o" && string[i]!= "u"){
+    
+  	//else if (string[i]!="e"){
+  	  //else if (string[i]!="i"){
+  	  	//else if (string[i]!="o"){
+  	  	//	else if (string[i]!="u"){
+
+console.log(string[i]);
+  }
+
+   
+}
