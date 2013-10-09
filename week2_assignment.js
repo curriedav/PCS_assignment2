8cfa8a3f1d9613564b01ac8776f4ab941c95f533
JFingert <joshuafingert@gmail.com>
2013-10-09 11:58:57 -0700

    Update week2_assignment.js
    Revision

--- week2_assignment.js
+++ week2_assignment.js
@@ -1,11 +1,25 @@
 var string = "Marbles is a chicken";
+var que;
 
+
+
+		
 for (var i = string.length; i>=0; i--){
+      
+	   
 
-  if (string[i]!= "a" && string[i]!= "e" && string[i]!= "i" && string[i]!= "o" && string[i]!= "u"){
-    
-  
+	  if (string[i]!= "a" && string[i]!= "e" && string[i]!= "i" && string[i]!= "o" && string[i]!= "u"){
+	    
+	    que = que + string[i];
 
-console.log(string[i]);
-  }
-}
+	    
+	   }
+
+
+ 		
+
+ }
+ console.log(que + string[i]);
+
+
+ 		
